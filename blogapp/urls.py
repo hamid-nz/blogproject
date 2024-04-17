2c072adfba0e9d590fec3115fb9cf717d578a461
Hamid Nazir <155911799+hamid-nz@users.noreply.github.com>
2024-04-17 23:43:05 +0500

    Update urls.py
    ...

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -6,11 +6,17 @@ urlpatterns = [
     path( '', HomeView.as_view(), name='home' ),
     path( 'post/<slug:post_url>/', SinglePostView.as_view(), name='single_post' ),
     path( 'category/<slug:category_url>/', get_blog_by_category, name='single_category' ),
-    # path( 'category/<slug:category_url>/', SingleCategoryView.as_view(), name='single_category' ),
+    
+#User pages
     path('sign-up/', sign_up, name='sign-up'),   
     path('sign-in/', sign_in, name='sign-in'),   
     path('sign-out/', sign_out, name='sign-out'),   
     path('account/', Account, name='account'),   
+#essential pages
+    path( 'about/', AboutView.as_view(), name='about' ),
+    path( 'contact/', ContactView.as_view(), name='contact' ),
+    path( 'desclaimer/', DisclaimerView.as_view(), name='desclaimer' ),
+    path( 'terms-conditions/', TCView.as_view(), name='termcondition' ),
 
 ]
 
